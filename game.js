34241ce3b7b64859bc04b7eb6f8ae7b513bbe952
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-12 14:59:35 -0400

    adds Game class

---
+++ game.js
@@ -0,0 +1,10 @@
+(function(root){
+    var Asteroids = root.Asteroids = (root.Asteroids || {});
+    
+    var Game = Asteroids.Game = function(ctx) {
+        this.ctx = ctx;
+        this.asteroids = [];
+    }
+    
+ 
+})(this);
