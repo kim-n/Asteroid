8ebbf1d38f4f4b6811a1b067d8b36335b65c67f4
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-12 14:29:19 -0400

    adds MovingObject class

---
+++ moving_object.js
@@ -0,0 +1,8 @@
+"use strict"
+
+var MovingObject = function(pos, vel) {
+    this.pos = pos;  // Array [x,y]
+    this.vel = vel;  // Array [x,y]
+    this.radius = radius;
+    this.color = color;
+}
