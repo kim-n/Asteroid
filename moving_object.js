7afb9e4c717faff701b0d9b978b0f6ada5a20bff
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-12 14:30:52 -0400

    adds MovingObject#draw()

--- moving_object.js
+++ moving_object.js
@@ -11,3 +11,18 @@ MovingObject.prototype.move = function() {
     this.pos[0] += this.vel[0]  // update x pos
     this.pos[1] += this.vel[1]  // update y pos
 }
+
+MovingObject.prototype.draw = function(ctx) {
+    ctx.beginPath();
+    // (x, y, radius, start[RADIANS], end[RADIANS], clockwise?)
+    ctx.arc(
+        this.pos[0],
+        this.pos[1],
+        this.radius,
+        0,
+        2 * Math.PI,
+        false
+    );
+    ctx.strokeStyle = this.color ;
+    ctx.stroke();
+}
