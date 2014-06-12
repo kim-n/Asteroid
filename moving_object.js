add6be3005edbca85bda418ea889be653238ecff
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-12 14:50:19 -0400

    moves MovingObject to Asteroids namespace

--- moving_object.js
+++ moving_object.js
@@ -1,39 +1,41 @@
-"use strict"
+(function(root){
+    var Asteroids = root.Asteroids = (root.Asteroids || {});
+    
+    var MovingObject = Asteroids.MovingObject = function(pos, vel) {
+        this.pos = pos;  // Array [x,y]
+        this.vel = vel;  // Array [x,y]
+        this.radius = radius;
+        this.color = color;
+    }
 
-var MovingObject = function(pos, vel) {
-    this.pos = pos;  // Array [x,y]
-    this.vel = vel;  // Array [x,y]
-    this.radius = radius;
-    this.color = color;
-}
+    MovingObject.prototype.move = function() {
+        this.pos[0] += this.vel[0]  // update x pos
+        this.pos[1] += this.vel[1]  // update y pos
+    }
 
-MovingObject.prototype.move = function() {
-    this.pos[0] += this.vel[0]  // update x pos
-    this.pos[1] += this.vel[1]  // update y pos
-}
-
-MovingObject.prototype.draw = function(ctx) {
-    ctx.beginPath();
-    // (x, y, radius, start[RADIANS], end[RADIANS], clockwise?)
-    ctx.arc(
-        this.pos[0],
-        this.pos[1],
-        this.radius,
-        0,
-        2 * Math.PI,
-        false
-    );
-    ctx.strokeStyle = this.color ;
-    ctx.stroke();
-}
+    MovingObject.prototype.draw = function(ctx) {
+        ctx.beginPath();
+        // (x, y, radius, start[RADIANS], end[RADIANS], clockwise?)
+        ctx.arc(
+            this.pos[0],
+            this.pos[1],
+            this.radius,
+            0,
+            2 * Math.PI,
+            false
+        );
+        ctx.strokeStyle = this.color ;
+        ctx.stroke();
+    }
 
 
-var distance = function(objA, objB){
-    return Math.sqrt(Math.pow(objA[0]-objB[0],2) + Math.pow(objA[1]-objB[1],2));
-}
+    var distance = function(objA, objB){
+        return Math.sqrt(Math.pow(objA[0]-objB[0],2) + Math.pow(objA[1]-objB[1],2));
+    }
 
-MovingObject.prototype.isCollidedWith = function(otherObject) {
-    var dist = distance(this.pos, otherObject.pos);
-    var radiiSum = this.radius + otherObject.radius;
-    return radiiSum > dist;
-}
+    MovingObject.prototype.isCollidedWith = function(otherObject) {
+        var dist = distance(this.pos, otherObject.pos);
+        var radiiSum = this.radius + otherObject.radius;
+        return radiiSum > dist;
+    }
+})(this);
