bd38c6e7966c4bc7b6aecc29e383c2ddea8cb549
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-12 14:29:55 -0400

    adds MovingObject#move()

--- moving_object.js
+++ moving_object.js
@@ -6,3 +6,8 @@ var MovingObject = function(pos, vel) {
     this.radius = radius;
     this.color = color;
 }
+
+MovingObject.prototype.move = function() {
+    this.pos[0] += this.vel[0]  // update x pos
+    this.pos[1] += this.vel[1]  // update y pos
+}
