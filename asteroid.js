860bd06c962d9730f6fc774f79315bcb180588a3
Kimberly Narine <kimberlyonarine@gmail.com>
2014-06-14 01:39:29 -0400

    astroids spawn on borders of screen

--- asteroid.js
+++ asteroid.js
@@ -11,9 +11,21 @@
     Asteroid.RADIUS = 20;
     Asteroids.MAX_SPEED = 10;
     
-    var randomPosition = function(xMax, yMax){
-        return [Math.floor(Math.random() * xMax),
-                Math.floor(Math.random() * yMax)]
+    var randomPosition = function(xMax, yMax) {
+      var rand = Math.floor(Math.random() * 4);
+      var x = Math.floor(Math.random() * xMax)
+      var y = Math.floor(Math.random() * yMax)
+      
+      switch(rand) {
+      case 0:
+          return [0, y];
+      case 1:
+          return [xMax, y];
+      case 2:
+          return [x, 0];
+      case 3:
+          return [x, yMax];
+      }
     }
     
     var randomVelocity = function(xMax, yMax){
@@ -23,10 +35,11 @@
     
     Asteroid.randomAsteroid = function(dimX, dimY) {
         var pos = randomPosition(dimX, dimY);
-        var vel = randomVelocity(Asteroids.MAX_SPEED, Asteroids.MAX_SPEED);
+        var vel = randomVelocity(Asteroids.MAX_SPEED, Asteroids.MAX_SPEED);        
         return new Asteroid(pos, vel);
     }
-    
+
+
 })(this);
 
 console.log("asteroid.js loaded")
